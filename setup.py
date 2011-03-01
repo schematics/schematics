b60128f79eb1cce8b191d2d5701418a16f42413b
James Dennis <jd@j2labs.net>
2011-02-28 19:48:53 -0500

    increment version

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@
 from distutils.core import setup
  
 setup(name='dictshield',
-      version='0.1',
+      version='0.1.1',
       description='Python Library for typed dictionaries... sorta',
       author='James Dennis',
-      author_email='james@extension.fm',
-      url='http://github.com/extensionfm/dictshield',
+      author_email='jdennis@gmail.com',
+      url='http://github.com/j2labs/dictshield',
       packages=['dictshield'])
