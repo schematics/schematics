16dd2d7eb6d60c788e4e5ec37c6d700e6d048f7d
James Dennis <jd@j2labs.net>
2011-06-13 17:15:30 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
  
 setup(name='dictshield',
-      version='0.2.2',
+      version='0.2.3',
       description='Python Library for typed dictionaries... sorta',
       author='James Dennis',
       author_email='jdennis@gmail.com',
