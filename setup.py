f15db565367723b16e951396e494ff40c6ab690d
James Dennis <jdennis@gmail.com>
2011-08-29 05:01:39 -0400

    DictShield is has earned 0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
  
 setup(name='dictshield',
-      version='0.2.8',
+      version='0.3.0',
       description='Python Library for typed dictionaries... sorta',
       author='James Dennis',
       author_email='jdennis@gmail.com',
