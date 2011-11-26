ce5c441e46d0b2cf291f0c12e4700441bbc6e482
James Dennis <jd@j2labs.net>
2011-11-26 13:37:04 -0500

    Updated ObjectIdField to retain previous behavior of not auto filling an ObjectIdField. It is often obtained after saving a Document to Mongo rather than autogenerating one.

--- dictshield/fields/mongo.py
+++ dictshield/fields/mongo.py
@@ -9,9 +9,12 @@ import bson
 class ObjectIdField(BaseField):
     """An field wrapper around MongoDB ObjectIds.  It is correct to say they're
     bson fields, but I am unaware of bson being used outside MongoDB.
+
+    `auto_fill` is disabled by default for ObjectIdField's as they are
+    typically obtained after a successful save to Mongo.
     """
 
-    def __init__(self, auto_fill=True, **kwargs):
+    def __init__(self, auto_fill=False, **kwargs):
         self.auto_fill = auto_fill
         super(ObjectIdField, self).__init__(**kwargs)    
 
@@ -19,7 +22,7 @@ class ObjectIdField(BaseField):
         """Convert any text values provided into Python UUID objects and
         auto-populate any empty values should auto_fill be set to True.
         """
-        if not value and self.audo_fill is True:
+        if not value and self.auto_fill is True:
             value = bson.objectid.ObjectId()
 
         if isinstance(value, (str, unicode)):
