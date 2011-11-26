69f0216e77a01bcf05513f16edd440c545af8f61
James Dennis <jd@j2labs.net>
2011-11-26 13:07:02 -0500

    Updated ObjectIdField to have default behavior like UUIDField.

--- dictshield/fields/mongo.py
+++ dictshield/fields/mongo.py
@@ -11,6 +11,22 @@ class ObjectIdField(BaseField):
     bson fields, but I am unaware of bson being used outside MongoDB.
     """
 
+    def __init__(self, auto_fill=True, **kwargs):
+        self.auto_fill = auto_fill
+        super(ObjectIdField, self).__init__(**kwargs)    
+
+    def __set__(self, instance, value):
+        """Convert any text values provided into Python UUID objects and
+        auto-populate any empty values should auto_fill be set to True.
+        """
+        if not value and self.audo_fill is True:
+            value = bson.objectid.ObjectId()
+
+        if isinstance(value, (str, unicode)):
+            value = bson.objectid.ObjectId(unicode(value))
+
+        instance._data[self.field_name] = value
+    
     def _jsonschema_type(self):
         return 'string'
 
