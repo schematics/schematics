508edef3de231f5241b52871ba71e5c524137261
Mickaël <desfrenes@gmail.com>
2012-02-07 22:14:26 +0100

    Make DatetimeField's __set__ compatible with the default string representation of Python's standard datetime:
    str(datetime_instance) defaults to YYYY-MM-DD HH:MM:SS.mmmmmm, not YYYY-MM-DDTHH:MM:SS.mmmmmm. Adding the T should help conversion.

--- dictshield/fields/base.py
+++ dictshield/fields/base.py
@@ -557,7 +557,7 @@ class DateTimeField(BaseField):
             return
 
         if isinstance(value, (str, unicode)):
-            value = DateTimeField.iso8601_to_date(value)
+            value = DateTimeField.iso8601_to_date("T".join(value.split(" ")))
 
         instance._data[self.field_name] = value
 
