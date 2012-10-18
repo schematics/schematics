1c121d39f31456b322661eb9c6b7bab02995973e
James Rowe <jnrowe@gmail.com>
2012-10-18 11:29:32 +0100

    Convert with 2to3 when installing with Python 3.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='schematics',
       license='BSD',
@@ -9,4 +9,5 @@ setup(name='schematics',
       author='James Dennis',
       author_email='jdennis@gmail.com',
       url='http://github.com/j2labs/schematics',
-      packages=['schematics', 'schematics.types'])
+      packages=['schematics', 'schematics.types'],
+      use_2to3=True)
