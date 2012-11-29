98b21c20dffb50b5a16f3b2933b82914cd3d0770
James Dennis <jd@j2labs.net>
2012-11-29 12:06:53 -0500

    Updated path to use the new validation system. Formatted the code along the way to fit within 80 characters width.

--- schematics/types/path.py
+++ schematics/types/path.py
@@ -1,21 +1,26 @@
 import os
 from os import path
 
-from .base import StringType, TypeException
+from schematics.validation import (TypeResult, FieldResult, OK)
+
 
 class PathType(StringType):
-    """
-    A path field.
-    You can use one or a combination of the following validation attributes to validate the path:
-      * exists. Asserts that the path exists.
-      * isfile. Asserts that if the path exists, it's a file.
-      * isdir. Asserts that if the path exists, it's a directory.
-      * access. Asserts that Python's os.access(path, access) == True. Use it to check for read/write/execute
-      *         permissions.
-      * can_create_or_write. Asserts that if the file exists it can be written to, and if it doesn't exist then its
+    """Implementation of a path field.
+
+    You can use one or a combination of the following validation attributes to
+    validate the path:
+    
+      * exists: Asserts that the path exists.
+      * isfile: Asserts that if the path exists, it's a file.
+      * isdir:  Asserts that if the path exists, it's a directory.
+      * access: Asserts that Python's os.access(path, access) == True. Use it
+                to check for read/write/execute permissions.
+      * can_create_or_write: Asserts that if the file exists it can be written
+                             to, and if it doesn't exist then its
       *                      parent directory exists and can be written to.
     """
-    def __init__(self, access=None, exists=False, isdir=False, isfile=False, can_create_or_write=False, **kwargs):
+    def __init__(self, access=None, exists=False, isdir=False, isfile=False,
+                 can_create_or_write=False, **kwargs):
         super(PathType, self).__init__(**kwargs)
         self.access = access
         self.exists = exists
@@ -25,39 +30,57 @@ class PathType(StringType):
 
     def validate(self, value):
         if not isinstance(value, basestring):
-            raise TypeException("value must be a string", self.field_name, value)
+            error_msg = "value must be a string"
+            return FieldResult(ERROR_TYPE_COERCION, error_msg,
+                               self.field_name, value)
 
         realpath = path.realpath(value)
         path_exists = path.exists(realpath)
 
         if self.exists and not path_exists:
-            raise TypeException("path does not exist", self.field_name, value)
+            error_msg = "path does not exist"
+            return FieldResult(ERROR_TYPE_COERCION, error_msg,
+                               self.field_name, value)
 
         if self.can_create_or_write:
             if path_exists and not os.access(realpath, os.W_OK):
-                raise TypeException("path is not writable", self.field_name, value)
+                error_msg = "path is not writable"
+                return FieldResult(ERROR_TYPE_COERCION, error_msg,
+                                   self.field_name, value)
             else:
                 parent_dir = path.dirname(realpath)
                 if not path.exists(path.dirname(realpath)):
-                    raise TypeException("parent directory does not exist", self.field_name, value)
+                    error_msg = "parent directory does not exist"
+                    return FieldResult(ERROR_TYPE_COERCION, error_msg,
+                                       self.field_name, value)
+
                 if not path.isdir(parent_dir):
-                    raise TypeException("parent directory is not a directory", self.field_name, value)
+                    error_msg = "parent directory is not a directory"
+                    return FieldResult(ERROR_TYPE_COERCION, error_msg,
+                                       self.field_name, value)
                 if not os.access(parent_dir, os.W_OK):
-                    raise TypeException("parent directory is not writable", self.field_name, value)
+                    error_msg = "parent directory is not writable"
+                    return FieldResult(ERROR_TYPE_COERCION, error_msg,
+                                       self.field_name, value)
 
         if path_exists and self.isfile and not path.isfile(realpath):
-            raise TypeException("path is not a file", self.field_name, value)
+            error_msg =  "path is not a file"
+            return FieldResult(ERROR_TYPE_COERCION, error_msg,
+                               self.field_name, value)
 
         if path_exists and self.isdir and not path.isdir(realpath):
-            raise TypeException("path is not a directory", self.field_name, value)
+            error_msg = "path is not a directory"
+            return FieldResult(ERROR_TYPE_COERCION, error_msg,
+                               self.field_name, value)
 
-        if self.access is not None:
-            if not os.access(realpath, self.access):
+        if self.access is not None and not os.access(realpath, self.access):
                 permissions = self._access_to_permission_list(self.access)
-                raise TypeException("path does not have one or more of the following permissions: {}".format(
-                        ", ".join(permissions)), self.field_name, value)
+                error_msg = "path one or more of the following permissions: {}"
+                return FieldResult(ERROR_TYPE_COERCION,
+                                   error_msg.format(", ".join(permissions))
+                                   self.field_name, value)
 
-        return value
+        return FieldResult(OK, 'success', self.field_name, value)
 
     def _access_to_permission_list(self, access):
         permissions = []
@@ -69,8 +92,13 @@ class PathType(StringType):
             permissions.append("executable")
         return permissions
 
+
 class ExecutablePathType(PathType):
-    """A field that stores a path to an executable file. It validates the path points to an existing file and that
-    the file can be executed by the current user."""
+    """A field that stores a path to an executable file. It validates the path
+    points to an existing file and that the file can be executed by the current
+    user.
+    """
     def __init__(self, **kwargs):
-        super(ExecutablePathType, self).__init__(access=os.R_OK | os.X_OK, exists=True, isfile=True, **kwargs)
+        super(ExecutablePathType, self).__init__(access=os.R_OK | os.X_OK,
+                                                 exists=True, isfile=True,
+                                                 **kwargs)
