6bf7f76b8552ea3e4ef523ad6117b967a7417f24
Ryan Olson <rmolson@gmail.com>
2013-02-05 00:53:28 -0700

    updated setup

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@ from setuptools import setup
 
 setup(name='schematics',
       license='BSD',
-      version='0.5.1alpha',
+      version='2.0.0dev',
       description='Structured Data for Humans',
-      author='James Dennis',
-      author_email='jdennis@gmail.com',
-      url='http://github.com/j2labs/schematics',
       packages=['schematics', 'schematics.types'])
