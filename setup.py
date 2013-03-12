c613a6ae1170f2cef2550d35ceefc91df1565baa
Ryan Olson <rmolson@gmail.com>
2013-03-12 00:47:50 -0600

    returned setup.py to default values

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ from setuptools import setup
 
 setup(name='schematics',
       license='BSD',
-      version='2.0.0dev',
+      version='0.5',
       description='Structured Data for Humans',
+      author='James Dennis',
+      author_email='jdennis@gmail.com',
+      url='http://github.com/j2labs/schematics',
       packages=['schematics', 'schematics.types'])
