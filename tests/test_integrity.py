4239df285dc12101371d9895d33ca1f423f6c453
Paul Eipper <paul@nkey.com.br>
2013-05-07 15:23:42 -0300

    Model setter integrity test

--- tests/test_integrity.py
+++ tests/test_integrity.py
@@ -20,3 +20,21 @@ class TestDataIntegrity(unittest.TestCase):
         p1 = Player({'code': 'invalid1'})
         self.assertRaises(ValidationError, p1.validate)
         self.assertEqual(p1.serialize(), {'code': None})
+
+    def test_dont_overwrite_with_invalid_data(self):
+        """
+        Model-level validators are black-boxes and we should not assume
+        that we can set the instance data at any time.
+
+        """
+        class Player(Model):
+            id = IntType()
+
+            def validate_id(self, context, value):
+                if self.id:
+                    raise ValidationError('Cannot change id')
+
+        p1 = Player({'id': 4})
+        p1.id = 3
+        self.assertRaises(ValidationError, p1.validate)
+        self.assertEqual(p1.id, 4)
