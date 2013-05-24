67468063c2cb5ad40a55773de606cd6747d6f17a
James Dennis <jd@j2labs.net>
2013-05-24 01:19:03 -0400

    Adding value coercion to the validate function

--- schematics/validate.py
+++ schematics/validate.py
@@ -46,6 +46,7 @@ def validate(model, raw_data, partial=False, strict=False, context=None):
             data[field_name] = None
         else:
             try:
+                value = field.convert(value)
                 field.validate(value)
             except BaseError as e:
                 errors[serialized_field_name] = e.messages
