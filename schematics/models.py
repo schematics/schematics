09b1a08f96feb2aee6c4a55ae55c6bea52eedaf8
Paul Eipper <paul@nkey.com.br>
2013-06-04 18:21:11 -0300

    Add `append_field` method to Model class

--- schematics/models.py
+++ schematics/models.py
@@ -179,6 +179,14 @@ class ModelMeta(type):
 
         return options_class(cls, **options_members)
 
+    def append_field(cls, name, field):
+        if isinstance(field, BaseType):
+            new_field = field.copy()
+            cls._fields[name] = new_field
+            setattr(cls, name, FieldDescriptor(name))
+        else:
+            raise TypeError('field must be of type %s' % BaseType)
+
     @property
     def fields(cls):
         return cls._fields
