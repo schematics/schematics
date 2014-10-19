a5113b912b2f4ab5a31143ce2997f7bae9ad4b9c
Simeon Visser <svisser@users.noreply.github.com>
2014-10-19 22:16:32 +0100

    Fixed project URL

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     description='Structured Data for Humans',
     author=u'James Dennis, Jökull Sólberg, Jóhann Þorvaldur Bergþórsson',
     author_email='jdennis@gmail.com, jokull@plainvanillagames.com, johann@plainvanillagames.com',
-    url='http://github.com/j2labs/schematics',
+    url='http://github.com/schematics/schematics',
     packages=['schematics', 'schematics.types', 'schematics.contrib'],
     classifiers=[
         'Environment :: Other Environment',
