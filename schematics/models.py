0521d09103c5d967050b205a85f2c9f71447f367
Kracekumar Ramaraju <me@kracekumar.com>
2015-02-12 10:55:53 +0530

    Fix circular import issue

--- schematics/models.py
+++ schematics/models.py
@@ -61,6 +61,7 @@ class FieldDescriptor(object):
         """
         Checks the field name against a model and sets the value.
         """
+        from .types.compound import ModelType
         field = instance._fields[self.name]
         if not isinstance(value, Model) and isinstance(field, ModelType):
             value = field.model_class(value)
@@ -430,6 +431,3 @@ class Model(object):
 
     def __unicode__(self):
         return '%s object' % self.__class__.__name__
-
-
-from .types.compound import ModelType
