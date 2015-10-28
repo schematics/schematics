7903d5d5082798ffdd443445637048fc146f513c
Jon Banafato <jon@jonafato.com>
2015-10-28 01:19:34 -0400

    Support Python 3.5
    - Add py35 to list of tox environments
    - Add Python 3.5 to language classifiers
    - Upgrade pytest version to address issues with Python 3.5
      (https://github.com/pytest-dev/pytest/pull/801)
    - Add apt addon required for Python 3.5

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
     cmdclass={
         'test': Tox,
