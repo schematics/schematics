30c02b977a6ed7e6374b4986d397eb5da2c6041e
Ke Li <kel@splunk.com>
2016-10-21 16:30:14 +0800

    Import types explicitly for better readability

--- schematics/__init__.py
+++ schematics/__init__.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import
 
 __version__ = '2.0.0.a1'
 
 from .models import Model, ModelMeta
+from . import types
 
 types.compound.Model = Model
 types.compound.ModelMeta = ModelMeta
